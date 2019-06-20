f64fb12dc9efb3ccee39f4aab2a870330f9f28d6
jorishr <joris@jorisr.com>
2019-06-20 18:50:27 +0200

    styling completed for: comments, forms and index page

--- app/seeds.js
+++ app/seeds.js
@@ -6,27 +6,27 @@ let starterData = [
     {
         name:'Vall d\'Aran',
         image: 'images/2875977867.png',
-        description: 'Beautiful lakes at great altitude. Great place for hiking and skying.'
+        description:  'Beautiful lakes at great altitude. Great place for hiking and skying. Lorem ipsum dolor sit amet, an nam nostrum rationibus definitionem, sint tollit verear nec ne. Vix ad nisl illud epicuri, eum an mundi homero signiferumque. Est homero minimum vituperatoribus in, sonet probatus id ius, cu assum probatus definiebas ius. Ius at postea omnesque. Nam in munere laoreet fuisset, vide iudico indoctum quo te, commune laboramus ex quo. Graeco atomorum mel in. Commune prodesset intellegebat ne eos, ut soluta invenire mel. Sed brute summo te. In vis nostro antiopam.'
     },
     {
         name:'La Mola',
         image: 'images/402270253.png',
-        description: 'Good facilities. Excellent starting point for mountain biking.'
+        description: 'Good facilities. Excellent starting point for mountain biking. Lorem ipsum dolor sit amet, an nam nostrum rationibus definitionem, sint tollit verear nec ne. Vix ad nisl illud epicuri, eum an mundi homero signiferumque. Est homero minimum vituperatoribus in, sonet probatus id ius, cu assum probatus definiebas ius. Ius at postea omnesque. Nam in munere laoreet fuisset, vide iudico indoctum quo te, commune laboramus ex quo. Graeco atomorum mel in. Commune prodesset intellegebat ne eos, ut soluta invenire mel. Sed brute summo te. In vis nostro antiopam.'
     },
     {
         name:'Delta de l\'Ebre',
         image: 'images/3837363146.png',
-        description: 'You can camp on the beach here! Great seafood and lot\'s of water sport activities.'
+        description: 'You can camp on the beach here! Great seafood and lot\'s of water sport activities. Lorem ipsum dolor sit amet, an nam nostrum rationibus definitionem, sint tollit verear nec ne. Vix ad nisl illud epicuri, eum an mundi homero signiferumque. Est homero minimum vituperatoribus in, sonet probatus id ius, cu assum probatus definiebas ius. Ius at postea omnesque. Nam in munere laoreet fuisset, vide iudico indoctum quo te, commune laboramus ex quo. Graeco atomorum mel in. Commune prodesset intellegebat ne eos, ut soluta invenire mel. Sed brute summo te. In vis nostro antiopam.'
     },
     {
         name:'Vall de Camprodon',
         image: 'images/31115219468.png',
-        description: 'Nice historic town with traditional gastronomy. Lots of hiking oportunities in the region.'
+        description: 'Nice historic town with traditional gastronomy. Lots of hiking oportunities in the region. Lorem ipsum dolor sit amet, an nam nostrum rationibus definitionem, sint tollit verear nec ne. Vix ad nisl illud epicuri, eum an mundi homero signiferumque. Est homero minimum vituperatoribus in, sonet probatus id ius, cu assum probatus definiebas ius. Ius at postea omnesque. Nam in munere laoreet fuisset, vide iudico indoctum quo te, commune laboramus ex quo. Graeco atomorum mel in. Commune prodesset intellegebat ne eos, ut soluta invenire mel. Sed brute summo te. In vis nostro antiopam.'
     },
 ];
 
 let dummyComment = {
-    text: 'We had a great time there! Highly recommend campsite. Good price point too!',
+    text: 'Lorem ipsum dolor sit amet, an nam nostrum rationibus definitionem, sint tollit verear nec ne. Vix ad nisl illud epicuri, eum an mundi homero signiferumque. Est homero minimum vituperatoribus in, sonet probatus id ius, cu assum probatus definiebas ius. Ius at postea omnesque. Nam in munere laoreet fuisset, vide iudico indoctum quo te, commune laboramus ex quo. Graeco atomorum mel in. Commune prodesset intellegebat ne eos, ut soluta invenire mel. Sed brute summo te. In vis nostro antiopam.',
     author: 'WhoKnows'
 }
 
