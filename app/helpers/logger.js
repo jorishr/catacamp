d57561bf288668b3de27245ef0390f5e8c971f41
jorishr <joris@jorisr.com>
2020-07-02 16:10:50 +0200

    basic error file log with Winston added

---
+++ app/helpers/logger.js
@@ -0,0 +1,35 @@
+const winston = require('winston');
+const options = {
+  file: {
+    level: 'info',
+    filename: `./logs/error.log`,
+    handleExceptions: true,
+    json: true,
+    maxsize: 5242880, // 5MB
+    maxFiles: 5,
+    colorize: true,
+  },
+  console: {
+    level: 'debug',
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+}; 
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.File(options.file)  
+  ],
+  exitOnError: false
+});
+ 
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+module.exports = logger;
--- app/middleware/error.js
+++ app/middleware/error.js
@@ -1,9 +1,12 @@
+const logger = require('../helpers/logger');
 //  make the error object available through res object
 //  set a generic server error status code if none is included in the err
 //  render the error.ejs page for the user
 //  if shouldRedirect is not defined in the error, send the original err data
-function errorHandler (err, req, res, next) {
-    console.error('\nError:\n', err.statusCode, err.messageForConsole);
+async function errorHandler (err, req, res, next) {
+    //console.error('\nError:\n', err.statusCode, err.messageForConsole);
+    await logger.error(
+      `${err.statusCode || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
     res.locals.error = err;
     if (!err.statusCode){err.statusCode = 500;}; 
     if (err.shouldRedirect) {
